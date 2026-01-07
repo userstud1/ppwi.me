04b69a6bac5b2f4512733c924eafe42187087fc9
userstud1 <richard.frant@usarb.md>
2026-01-07 17:41:57 +0200

    prepare to deploy

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ import nightwatchPlugin from 'vite-plugin-nightwatch'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/',
+  base: '/ppwi.me/',
   plugins: [
     vue(),
     vueJsx(),
